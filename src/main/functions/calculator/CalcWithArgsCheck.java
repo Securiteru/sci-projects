5f72c19341bbeb2244ce6743c6a42b4d0cc62ad4
Adrian Padurean <securiteradmin@gmail.com>
2018-08-07 07:38:54 +0300

    Final version2

--- src/main/functions/calculator/CalcWithArgsCheck.java
+++ src/main/functions/calculator/CalcWithArgsCheck.java
@@ -14,6 +14,16 @@ public class CalcWithArgsCheck {
     private int checkY = 0;
     private char z;
 
+
+
+    /**
+     * Function that checks the arguments provided by the users, to make sure they're valid inputs
+     *
+     * @param x first number to calculate
+     * @param y second number to calculate
+     * @param z the character used to calculate
+     */
+
     public void check(String x, String y, char z) {
 
         try {
@@ -27,6 +37,9 @@ public class CalcWithArgsCheck {
         }
 
         try {
+            if(z== '/' && checkY==0){
+                throw new IllegalArgumentException("Second input is 0");
+            }
            this.checkY = Integer.parseInt(y);
            this.yCorrect = true;//taking in an integer input throws NumberFormat Exception if not an integer
         } catch (NumberFormatException e) {   //is input is not an integer, occurs while parsing the command line input argument
@@ -35,12 +48,24 @@ public class CalcWithArgsCheck {
         } catch (ArrayIndexOutOfBoundsException e) { //as takes in input from command line which is stored to a Args array in main, if this array is null implies no input given
             System.out.println("Please enter a number on the command line");
         }
-        this.z=z;
-        if (xCorrect && yCorrect){calculateCheckOk(checkX,checkY,z);}
 
+        this.z=z;
+
+        if (xCorrect && yCorrect){
+            calculateCheckOk(checkX,checkY,z);
+        }
         }
 
-        public void calculateCheckOk(int checkX, int checkY, char z){
+    /**
+     * Function that executes the mathematical operation if the inputs were considered valid by the previous function's check.
+     *
+     * @param x first number to calculate
+     * @param y second number to calculate
+     * @param z the character used to calculate
+     * @return N/A
+     */
+
+        private void calculateCheckOk(int checkX, int checkY, char z){
             double result;
             if (xCorrect && yCorrect) {
                 switch (z) {
@@ -54,7 +79,7 @@ public class CalcWithArgsCheck {
                         result = checkX * checkY;
                         break;
                     case '/':
-                        result = checkX * checkY;
+                        result = checkX / checkY;
                         break;
                     default:
                         result = 0;
